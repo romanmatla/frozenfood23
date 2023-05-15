b6511bba188ae99ec8f517e202b1178bbea5fd72
Roman Matla <romaanek@wp.pl>
2023-05-15 13:47:18 +0200

    Implement sign out

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -13,7 +13,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('jesteś zalogowany jako ${user.email}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('jesteś zalogowany jako ${user.email}'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: const Text('Wyloguj'),
+            ),
+          ],
+        ),
       ),
     );
   }
