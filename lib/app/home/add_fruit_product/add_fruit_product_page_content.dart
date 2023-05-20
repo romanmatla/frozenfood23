57b6507d1a12795673ff4abd9f241aa5caa2d8f8
Roman Matla <romaanek@wp.pl>
2023-05-20 18:19:22 +0200

    Implement adding product

--- lib/app/home/add_fruit_product/add_fruit_product_page_content.dart
+++ lib/app/home/add_fruit_product/add_fruit_product_page_content.dart
@@ -14,15 +14,21 @@ class AddFruitProductPageContent extends StatefulWidget {
 
 class _AddFruitProductPageContentState
     extends State<AddFruitProductPageContent> {
-  final controller = TextEditingController();
+  // final controller = TextEditingController();
+
+  var categories = '';
+  var name = '';
+  var dateAdded = '';
+  var expirationDate = '';
+  var quantity = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.arrow_back_sharp, size: 30),
-        label: Text('Cofnij'),
+        icon: const Icon(Icons.arrow_back_sharp, size: 30),
+        label: const Text('Cofnij'),
 
         onPressed: () {
           Navigator.of(context).pop();
@@ -58,7 +64,7 @@ class _AddFruitProductPageContentState
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.white),
                 child: Center(
-                  child: Column(
+                  child: ListView(
                     children: [
                       Padding(
                         padding: const EdgeInsets.all(8.0),
@@ -77,13 +83,19 @@ class _AddFruitProductPageContentState
                               fontSize: 18, fontWeight: FontWeight.w300),
                         ),
                       ),
-                      Container(
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            children: [
-                              TextField(
-                                controller: controller,
+                      Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    name = newValue;
+                                  });
+                                },
+                                // controller: controller,
                                 decoration: const InputDecoration(
                                   labelText: 'Produkt',
                                   labelStyle: TextStyle(color: Colors.blue),
@@ -107,26 +119,151 @@ class _AddFruitProductPageContentState
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    quantity = newValue;
+                                  });
+                                },
+                                // controller: controller,
+                                decoration: const InputDecoration(
+                                  labelText: 'Ilość',
+                                  labelStyle: TextStyle(color: Colors.blue),
+                                  border: OutlineInputBorder(),
+                                  // border: InputBorder(),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 47, 151, 236),
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 248, 134, 3),
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    dateAdded = newValue;
+                                  });
+                                },
+                                // controller: controller,
+                                decoration: const InputDecoration(
+                                  labelText: 'Data dodania',
+                                  labelStyle: TextStyle(color: Colors.blue),
+                                  border: OutlineInputBorder(),
+                                  // border: InputBorder(),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 47, 151, 236),
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 248, 134, 3),
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                onChanged: (newValue) {
+                                  setState(() {
+                                    expirationDate = newValue;
+                                  });
+                                },
+                                // controller: controller,
+                                decoration: const InputDecoration(
+                                  labelText: 'Termon ważności',
+                                  labelStyle: TextStyle(color: Colors.blue),
+                                  border: OutlineInputBorder(),
+                                  // border: InputBorder(),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 47, 151, 236),
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 248, 134, 3),
+                                    ),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(15),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                FirebaseFirestore.instance
+                                    .collection('product')
+                                    .add(
+                                  {
+                                    'name': name,
+                                    'categories': 'Owoce',
+                                    'date added': dateAdded,
+                                    'expiration date': expirationDate,
+                                    'quantity': quantity,
+                                  },
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(12))),
+                                fixedSize: const Size(60, 60),
+                              ),
+                              child:
+                                  const Icon(Icons.arrow_back_sharp, size: 30),
+                            ),
+                          ],
                         ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          FirebaseFirestore.instance.collection('product').add(
-                            {
-                              'name': 'ddddd',
-                            },
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(12))),
-                          fixedSize: const Size(60, 60),
-                        ),
-                        child: const Icon(Icons.arrow_back_sharp, size: 30),
-                      ),
+                      // ElevatedButton(
+                      //   onPressed: () {
+                      //     FirebaseFirestore.instance.collection('product').add(
+                      //       {
+                      //         'name': name,
+                      //         'categories': 'Owoce',
+                      //         'date added': dateAdded,
+                      //         'expiration date': expirationDate,
+                      //       },
+                      //     );
+                      //   },
+                      //   style: ElevatedButton.styleFrom(
+                      //     shape: const RoundedRectangleBorder(
+                      //         borderRadius:
+                      //             BorderRadius.all(Radius.circular(12))),
+                      //     fixedSize: const Size(60, 60),
+                      //   ),
+                      //   child: const Icon(Icons.arrow_back_sharp, size: 30),
+                      // ),
                     ],
                   ),
                 ),
