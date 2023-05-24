c69173a9773ec34e4d736224437d00b537a2adbb
Roman Matla <romaanek@wp.pl>
2023-05-24 23:11:58 +0200

    Move MyApp to app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:frozen_food/app/features/home/home_page.dart';
-import 'package:frozen_food/app/features/login/login_page.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:frozen_food/app/app.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,40 +11,4 @@ void main() async {
   );
 
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Frozen_Food',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RootPage(),
-    );
-  }
-}
-
-class RootPage extends StatelessWidget {
-  const RootPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        final user = snapshot.data;
-        if (user == null) {
-          return  LoginPage();
-        }
-        return HomePage(user: user);
-      },
-    );
-  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:frozen_food/app/app.dart';
 
 import 'package:frozen_food/main.dart';
 
